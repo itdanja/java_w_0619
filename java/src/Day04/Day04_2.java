01d0d1c1705337d3c87d0c0b712e720126d57eff
itdanja <itdanj@kakao.com>
2021-06-27 13:13:16 +0900

    Day04

--- java/src/Day04/Day04_2.java
+++ java/src/Day04/Day04_2.java
@@ -92,13 +92,23 @@ System.out.println("\n-------------------------------------------");
 			
 		}
 		// 문제7 : 입력한 줄 만큼 별삼각형 출력 
-			/*  예 ) 5 
-			 		*
-			 	   ***
-			 	  *****
-			 	 *******
-			 	*********
+			/*  예 ) 5 					b				s	
+			 		*		i = 1		b = 1 2 3 4		s = 1					1*2 = 2 -1
+			 	   ***		i = 2		b = 1 2 3		s = 1 2 3 				2*2 = 4 -1
+			 	  *****		i = 3 		b = 1 2			s = 1 2 3 4 5 			3*2	= 6 -1
+			 	 *******	i = 4		b = 1			s = 1 2 3 4 5 6 7		4*2 = 8 -1
+			 	*********	i = 5		b				s = 1 2 3 4 5 6 7 8 9 	5*2 = 10 -1
+			 							최대줄수-현재줄수		현재줄수 * 2 - 1
 			 */
+		System.out.print("문제7 정수 입력 : ");	int 문제7 = scanner.nextInt();
+		for( int i = 1 ; i<=문제7 ; i++ ) {
+			// 공백찍기
+			for( int b = 1 ; b<=문제7-i ; b++ ) {System.out.print(" ");}
+			// 별찍기 
+			for( int s = 1 ; s<= i*2-1 ; s++ ) {System.out.print("*");}
+			// 줄바꿈
+			System.out.println();
+		}
 		// 문제8 : 입력한 줄 만큼 별삼각형 출력 
 			/*  예 ) 5 
 			 		1
@@ -108,25 +118,56 @@ System.out.println("\n-------------------------------------------");
 			 	555555555
 			 */
 		
-		// 문제9 : 입력한 줄 만큼 별삼각형 출력 
-			/*  예 ) 5 
-			 	*********
-			 	 *******
-			 	  *****
-			 	   ***
-			 	    *
-			 */
+		System.out.print("문제8 정수 입력 : ");	int 문제8 = scanner.nextInt();
+		for( int i = 1 ; i<=문제8 ; i++ ) {
+			// 공백찍기
+			for( int b = 1 ; b<=문제8-i ; b++ ) {System.out.print(" ");}
+			// 별찍기 
+			for( int s = 1 ; s<= i*2-1 ; s++ ) {System.out.print(i);}
+			// 줄바꿈
+			System.out.println();
+		}
 		
-		// 문제10 : X 모양 출력 
+		// 문제9 : 입력한 줄 만큼 별삼각형 출력 
+			/*  예 ) 5 			b				S
+			 	*********		b = 			s = 1 2 3 4 5 6 7 8 9		5*2	10
+			 	 *******		b = 1			s = 1 2 3 4 5 6 7			5*2	10
+			 	  *****			b = 1 2			s = 1 2 3 4 5				5*2	10
+			 	   ***			b = 1 2 3 		s = 1 2 3					5*2	10
+			 	    *			b = 1 2 3 4		s = 1						5*2	10
+			 	    			b<=i-1			s = 최대줄수*2 - ( 현재줄수*2-1 )
+			 */
+		System.out.print("문제9 정수 입력 : ");	int 문제9 = scanner.nextInt();
+		for( int i = 1 ; i<=문제9 ; i++ ) {
+			// 공백찍기
+			for( int b = 1 ; b<=i-1 ; b++ ) {System.out.print(" ");}
+			//별찍기
+			for( int s = 1 ; s<= 문제9*2 - (i*2-1) ; s++ ) {System.out.print("*");}
+			//줄바꿈
+			System.out.println();
+		}
+		
+		// 문제10 : X 모양 출력  // 홀수 정수로 입력받기 
 			/*  예 ) 5 
-			 	 *     *
+
 			 	  *   *
 			       * *
 			        *
 			 	   * *
 			 	  *   *
-			 	 *     *
 			 */
+		System.out.print("문제10 정수 입력 : ");	int 문제10 = scanner.nextInt();
+		
+		for( int i = 1 ; i<=문제10 ; i++ ) { // i = 행 = 가로 
+			for( int j = 1 ; j<=문제10 ; j++ ) { // j = 열 = 세로 
+				if( i == j ) System.out.print("*"); // 행 = 열 동일한 경우 별찍기 
+				else if( (i+j) == (문제10+1) ) System.out.print("*"); // 행 과 열의 더한값이 최대줄수+1 과 동일한 경우 별찍기
+				else System.out.print(" ");
+			}
+			// 줄바꿈 
+			System.out.println();
+		}
+		
 			
 		
 		
---
+++ java/src/Day04/Day04_3.java
@@ -0,0 +1,89 @@
+package Day04;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Day04_3 {
+
+	// 가위바위보 게임 
+		// 1. 가위 바위 보 중 하나를 입력받아 승패 결정 
+		// 2. 컴퓨터는 랜덤 사용 
+		// 3. 게임 여러번 진행해서 최종 승리자 
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		int 게임횟수 = 0 ; 
+		int 플레이어이긴수 = 0;
+		int 컴퓨터가이긴수 = 0;
+		
+		while( true ) { // 무한루프 
+
+			// 플레이어
+			System.out.print("가위[0]바위[1]보[2] 게임종료[4] 중 선택 : "); int 플레이어 = scanner.nextInt();
+			if( 플레이어 == 0 )		System.out.println("--->플레이어는 가위 을 선택했습니다");
+			else if( 플레이어 == 1 )	System.out.println("--->플레이어는 바위 을 선택했습니다 ");
+			else if( 플레이어 == 2 )	System.out.println("--->플레이어는 보 을 선택했습니다");
+			else if( 플레이어 == 4 ) 	break;
+			else System.out.println("[경고] 알수 없는 번호 입니다 ");
+			
+			// 게임횟수 증가
+				게임횟수++;
+			
+			// 컴퓨터  => 랜덤 이용한 난수생성 [ Random : 랜덤 관련된 메소드 제공  ]
+			Random random = new Random(); // 랜덤 객체 만들기 
+			int 컴퓨터 = random.nextInt(3); // 랜덤 객체에서 난수 가져오기 [ .nextInt(숫자)   0~숫자전까지 난수생성 ]
+			
+			if( 컴퓨터 == 0 )			System.err.println("--->컴퓨터는 가위 을 선택했습니다");
+			else if( 컴퓨터 == 1 )	System.err.println("--->컴퓨터는 바위 을 선택했습니다 ");
+			else if( 컴퓨터 == 2 )	System.err.println("--->컴퓨터는 보 을 선택했습니다");
+			
+			// 이기는 경우수 
+					// 플레이어 이기는 수 [ 0 , 2 ] [ 1 , 0 ] [ 2 , 1 ]
+					// 비기는 경우의수 [ 0 , 0 ] [ 1 , 1 ] [ 2  , 2 ]
+					// 컴퓨터가 이기는 수 [ 그외 ]		
+			if( (플레이어==0 && 컴퓨터==2) || (플레이어==1 && 컴퓨터==0) || (플레이어==2 && 컴퓨터==1)  ) {
+				System.out.println("--->플레이어 승리");
+				// 플레이어 이긴수 증가 
+				플레이어이긴수++;
+			}else if( (플레이어==0 && 컴퓨터==0) || (플레이어==1 && 컴퓨터==1) || (플레이어==2 && 컴퓨터==2)  ) {
+				System.out.println("--->무승부");
+			}
+			else {
+				System.out.println("--->컴퓨터 승리");
+				// 컴퓨터 이긴수 증가 
+				컴퓨터가이긴수++;
+			}
+			
+		} // w end 
+		
+		System.out.println("----->[게임종료]");
+		System.out.println("-----> 플레이 게임횟수 : " + 게임횟수);
+		System.out.println("-----> 플레이어 이긴수 : " + 플레이어이긴수);
+		System.out.println("-----> 컴퓨터 이긴수 : " + 컴퓨터가이긴수);
+		
+		if( 플레이어이긴수 > 컴퓨터가이긴수 ) 
+			System.out.println("-------> 최종 승리자 : 플레이어 ");
+		else if( 플레이어이긴수 < 컴퓨터가이긴수 )
+			System.out.println("-------> 최종 승리자 : 컴퓨터 ");
+		else
+			System.out.println("-------> 최종 승리자 : 무승부 ");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+}
