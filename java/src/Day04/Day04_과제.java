abd6ccc9f7475063020151b66a22cdb98066dfc2
itdanja <78428879+itdanja@users.noreply.github.com>
2021-07-04 02:04:21 +0900

    Update Day04_과제.java

--- java/src/Day04/Day04_과제.java
+++ java/src/Day04/Day04_과제.java
@@ -1,8 +1,8 @@
 package Day04;
 
-public class Day04_°úÁ¦ {
+public class Day04_Â°ÃºÃÂ¦ {
 	
-/*	°úÁ¦1
+/*	Â°ÃºÃÂ¦1
 	  	*        *
 		**      **
 		***    ***
@@ -13,7 +13,7 @@ public class Day04_°úÁ¦ {
 		**      **
 		*        *
 */
-/* °úÁ¦2
+/* Â°ÃºÃÂ¦2
 	                   *                        
                       * *                       
                      *****                      
@@ -45,3 +45,104 @@ public class Day04_°úÁ¦ {
 	
 
 }
+
+
+/*
+
+import java.util.Scanner;
+
+public class Main {
+
+   public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      int N = scan.nextInt();
+      int col = 0;
+      int space = 2;
+
+      
+      for(int i=1; i<=N; i++) {
+         for(int j=0; j<i; j++)
+            System.out.print("*");
+         
+         for(int j=(i-1)*2; j<(N*2)-2; j++)
+            System.out.print(" ");
+
+         for(int j=0; j<i; j++)
+            System.out.print("*");
+         
+         System.out.println();
+      }
+
+      
+      for(int i=N; i<(N*2)-1; i++) {
+         for(int j=col; j<N-1; j++)
+            System.out.print("*");
+         
+         for(int j=0; j<space; j++)
+            System.out.print(" ");
+         
+         for(int j=col; j<N-1; j++)
+            System.out.print("*");
+         
+         System.out.println();
+         col ++;
+         space += 2;
+      }
+
+      scan.close();
+   }
+
+}
+
+*/
+
+
+
+
+/*
+import java.util.Scanner;
+ 
+public class Main {
+     
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+         
+        String map[] = new String[n];
+        map[0] = "  *  ";
+        map[1] = " * * ";
+        map[2] = "*****";
+         
+        for (int k = 1; 3 * (int)Math.pow(2, k) <= n; ++k) {
+            makeBigStar(k, map);
+        }
+         
+        for (int i = 0; i < n; ++i) {
+            System.out.println(map[i]);
+        }
+    }
+     
+    private static void makeBigStar(int k, String map[]) {
+        int bottom = 3 * (int)Math.pow(2, k);
+        int middle = bottom / 2;
+         
+        for (int i = middle; i < bottom; ++i) {
+            map[i] = map[i - middle] + " " + map[i -middle];
+        }
+         
+        String space = "";
+        while (space.length() < middle) {
+            space += " ";
+        }
+        for (int i = 0; i < middle; ++i) {
+            map[i] = space + map[i] + space;
+        }
+    }
+}
+
+
+*/
+
+
+
