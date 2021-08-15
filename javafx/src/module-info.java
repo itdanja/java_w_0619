47bc3c33c820c94aa1966a1375ad502d87ebda91
itdanja <78428879+itdanja@users.noreply.github.com>
2021-08-15 11:27:59 +0900

    day16

--- javafx/src/module-info.java
+++ javafx/src/module-info.java
@@ -3,8 +3,10 @@ module javafx {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.graphics;
+	requires java.sql;
 	
 	// 현 패키지내에서 사용할 외부 라이브러리 연결 
 	opens application to javafx.controls , javafx.graphics, javafx.fxml;
-	opens Member to javafx.controls , javafx.graphics , javafx.fxml;
+	opens Member to javafx.controls , javafx.graphics , javafx.fxml , java.sql;
+
 }
